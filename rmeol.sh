cdcd5db5115e2084d0440d0e1789b9a651641254
Heechan Park <phcphc82@gmail.com>
2020-10-09 16:32:34 +0900

    Add rmeol.sh
    rmeol.sh removes the last character when it is a eol.

---
+++ rmeol.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+has_eol() {
+  tail -1 "$1" | od -An -tx1 | grep -q 0a
+}
+
+get_size() {
+  ls -l "$1" | awk '{print $5}'
+}
+
+remove_eol() {
+  has_eol "$1" || exit 0
+
+  local tmp=tmp.$$
+  local size=$(get_size "$1")
+
+  dd if="$1" of=$tmp bs=$((size - 1)) count=1
+  mv -f $tmp "$1" || rm -f $tmp
+}
+
+for f in "$@"; do
+  remove_eol "$f"
+done
